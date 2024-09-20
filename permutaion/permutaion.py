599dcaf0e4a7fad5c35d59bf5885e6f944da371b
albpy <albinjos64@gmail.com>
2024-09-20 18:40:12 +0530

    added permutation python solution

--- permutaion/permutaion.py
+++ permutaion/permutaion.py
@@ -1,6 +1,21 @@
 ################In place swapping
 
 def count_to_make_permute(arr: list) -> int:
+    operations = 0
+    arr.sort()
+    ind = 0
+    for i in range(len(arr)):
+        ind+=1
+        if arr[i] > 0 and len(arr) < 10e5:
+            while (arr[i]!=ind):
+                if arr[i] < ind:
+                    arr[i] += 1
+                    operations += 1
+                if arr[i] > ind:
+                    arr[i] -= 1
+                    operations += 1
+                else:
+                    operations += 0
 
     return operations
 
